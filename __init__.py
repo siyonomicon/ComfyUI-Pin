2942318b8573881cdbf8eb5c21eaa903f39efae5
siyonomicon <221497162+siyonomicon@users.noreply.github.com>
2025-07-21 19:17:56 +0700

    Remove __pycache__ from tracking

--- __init__.py
+++ __init__.py
@@ -2,6 +2,8 @@ import server
 from aiohttp import web
 
 IMAGE_URLS = [
+    "https://plus.unsplash.com/premium_photo-1752155109947-539988d49e5d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8",
+    "https://plus.unsplash.com/premium_photo-1752155109947-539988d49e5d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8",
     "https://plus.unsplash.com/premium_photo-1752155109947-539988d49e5d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8"
 ]
 
